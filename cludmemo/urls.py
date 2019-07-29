2dd581bd1d4d0a68cf56e4a8f901cd5f58d6e296
YuRiSong <dbfldi10@naver.com>
2019-07-30 04:06:49 +0900

    first commit

---
+++ cludmemo/urls.py
@@ -0,0 +1,29 @@
+"""cludmemo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import cludmemoapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', cludmemoapp.views.home, name="home"),
+    path('blog/<int:blog_id>/', cludmemoapp.views.detail, name="detail"),
+    path('new/', cludmemoapp.views.new, name="new"),
+    path('create', cludmemoapp.views.create, name="create"),
+    path('blog/<int:blog_id>/delete/', cludmemoapp.views.delete, name="delete"),
+    path('blog/<int:blog_id>/edit/', cludmemoapp.views.edit, name="edit"),
+    path('blog/<int:blog_id>/update/', cludmemoapp.views.update, name="update"),
+]
---
+++ cludmemoapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CludmemoappConfig(AppConfig):
+    name = 'cludmemoapp'
